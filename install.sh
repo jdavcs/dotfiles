eaf8d2e810fbce07682656700aaebea66848b293
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2017-08-13 15:15:51 -0500

    Modify install.sh
    Replaced 'dotfiles' with $PWD: name of dotfiles directory
    should not be hardcoded into instllation script.

--- install.sh
+++ install.sh
@@ -1,48 +1,34 @@
-BASEDIR=dotfiles
-VIMDIR=vim
-VIMBLINK=${VIMDIR}/bundle
-
-#change this when your dev directory changes
-VIMBTARGET=~/0dev/vim-bundle
+readonly BASEDIR="$PWD"
+readonly VIMDIR='vim'
 
 # Make a symlink from home directory to dotfiles repo
 #   if 2 args supplied: use 2nd for target
 function make_homedir_link {
-    if [ $# -eq 2 ]; then
-        TARGET=$2
+    local target
+    if [[ $# -eq 2 ]]; then
+        target="$2"
     else
-        TARGET=$1
+        target="$1"
     fi
-
-    make_link ~/$1 ${BASEDIR}/$TARGET
-}
-
-# Make a synlink to a vim plugin
-function make_vimplugin_link {
-    make_link ${VIMBLINK}/$1 ${VIMBTARGET}/$1
+    make_link ~/"$1" "${BASEDIR}"/"$target"
 }
 
 # link helper
 function make_link {
-    echo ln -s $2 $1
-    if [ ! -L "$1" ] 
-    then
-        echo "linking " $1
-        ln -s $2 $1
+    echo ln -s "$2" "$1"
+    if [[ ! -L "$1" ]]; then
+        echo "linking " "$1"
+        ln -s "$2" "$1"
     else 
         echo "link to $1 already exists"
     fi
 }
-
 
 make_homedir_link .bashrc bashrc
 make_homedir_link .dircolors dircolors
 make_homedir_link .gitconfig gitconfig
 make_homedir_link .inputrc inputrc
 make_homedir_link .vimrc vimrc
-make_homedir_link .vim $VIMDIR
+make_homedir_link .vim "${VIMDIR}"
 make_homedir_link git-completion.bash
 make_homedir_link git-prompt.sh
-
-#make_vimplugin_link vim-write
-#make_vimplugin_link vim-journal
