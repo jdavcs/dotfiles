ac5cd43d19f444d5345d66350f1fd941f53a4dad
John Davis <jdavcs@gmail.com>
2025-12-23 15:25:33 -0500

    Update misc.

--- setup.py
+++ setup.py
@@ -2,26 +2,24 @@ import os
 
 
 LINKS = {
-    '.angular-config.json': 'angular-config.json',
+    '.bash_profile': 'bash/bashrc',
     '.bashrc': 'bash/bashrc',
-    '.condarc': 'condarc',
-    '.config/Code/User/settings.json': 'vscode-settings.json',
-    '.config/pgcli/config': 'pgcli-config',
-    '.dircolors': 'dircolors',
     '.editrc': 'editrc',
-    '.flake8': 'flake8',
-    '.gitconfig': 'git/gitconfig',
-    '.gitignmore_global': 'git/gitignore_global',
+    '.dircolors': 'dircolors',
     '.inputrc': 'inputrc',
-    '.ipython/profile_default/ipython_config.py': 'ipython/ipython_config.py',
-    '.ipython/profile_default/startup/jk.py': 'ipython/startup/jk.py',
-    '.irbrc': 'irbrc',
-    '.myclirc': 'mycli',
-    '.pdbrc': 'pdbrc',
-    '.pylintrc': 'pylintrc',
-    '.tmux.conf': 'tmux.conf',
     '.vim': 'vim',
     '.vimrc': 'vimrc',
+    '.tmux.conf': 'tmux.conf',
+    '.gitconfig': 'git/gitconfig',
+    '.gitignmore_global': 'git/gitignore_global',
+    '.ipython/profile_default/ipython_config.py': 'ipython/ipython_config.py',
+    '.ipython/profile_default/startup/jk.py': 'ipython/startup/jk.py',
+    '.pdbrc': 'pdbrc',
+    '.pylintrc': 'pylintrc',
+    '.flake8': 'flake8',
+    '.condarc': 'condarc',
+    '.config/Code/User/settings.json': 'vscode-settings.json',
+    '.irbrc': 'irbrc',
     }
 
 HOME = os.path.expanduser('~')
@@ -31,7 +29,7 @@ def setup():
     for name, target in LINKS.items():
         name = os.path.join(HOME, name)
         target = os.path.join(DOTFILES, target)
-        stm = 'ln -snf {} {}'.format(target, name)
+        stm = f'ln -snf {target} {name}'
         print(stm)
         os.system(stm)
 
